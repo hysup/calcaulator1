69e1daadaa4607fafe22a9562279b3472139595f
hysup <162294237+hysup@users.noreply.github.com>
2024-04-25 21:25:39 +0900

    calc Lv1, Lv2 구현완료

--- Main.kt
+++ Main.kt
@@ -4,18 +4,14 @@ fun main() {
     val num1 = readln().toInt()
     val operator = readln().toString()
     val num2 = readln().toInt()
-
-    if (operator == "+") {
-        println("${num1 + num2}")
-    } else if (operator == "-") {
-        println("${num1 - num2}")
-    } else if (operator == "*") {
-        println("${num1 * num2}")
-    } else if (operator == "/") {
-        println("${num1 / num2}")
-    } else if (operator == "%") {
-        println("나머지는 ${num1 % num2}")
-    } else {
-        println("잘못된 연산입니다")
+    var result = 0
+    when(operator) {
+        "+" -> result = Calculator().add(num1, num2)
+        "-" -> result = Calculator(). subtract(num1, num2)
+        "*" -> result = Calculator(). multiply(num1, num2)
+        "/" -> result = Calculator(). divide(num1, num2)
+        else -> println("Invalid operator")
     }
+
+println("결과는 ${num1} ${operator} ${num2} = ${result}")
 }
---
+++ calculator.kt
@@ -0,0 +1,28 @@
+
+
+class Calculator{
+    fun add(num1: Int, num2: Int): Int {
+            return num1 + num2
+    }
+
+    fun subtract(num1: Int, num2: Int): Int {
+        return num1 - num2
+
+    }
+
+    fun multiply(num1: Int, num2: Int): Int {
+        return num1 * num2
+    }
+
+    fun divide(num1: Int, num2: Int): Int {
+        return num1 / num2
+    }
+
+    fun remainder(num1: Int, num2: Int): Int {
+        return num1 % num2
+    }
+
+
+
+
+}
