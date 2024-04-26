6f6f4c6d7813ebca2a1350b972ef378d1dbf4987
hysup <162294237+hysup@users.noreply.github.com>
2024-04-26 12:55:57 +0900

    LV.3 구현에서 클래스 간의 관계맺기에서 해답을 찾지못함.

---
+++ calc.kt
@@ -0,0 +1,10 @@
+package operations
+
+import Addoperation
+
+class calc {
+    fun addoperation
+    fun subtractaction
+    fun multiplication
+    fun division
+}
---
+++ operations.kt
@@ -0,0 +1,21 @@
+class Addoperation{
+    fun addoperate(addoperation: Addoperation, num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
+class SubtractOperation{
+    fun subtractoperate(subtractOperation: SubtractOperation,num1: Int , num2: Int): Int {
+        return num1 - num2
+    }
+}
+class MultiplyOperation{
+    fun multiplyoperate(multiplyOperation: MultiplyOperation, num1: Int , num2: Int): Int {
+        return num1 * num2
+    }
+}
+class DivideOperation {
+    fun divideoperate(divideOperation: DivideOperation, num1: Int, num2: Int): Int {
+        return num1 / num2
+    }
+}
+
